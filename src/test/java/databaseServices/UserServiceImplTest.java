04aadb42e3acaa18466650ccb26ba1070b3d0073
AnshuMn123 <asingh@taxilla.com>
2023-07-17 23:36:32 +0530

    methods for test instances

---
+++ src/test/java/databaseServices/UserServiceImplTest.java
@@ -0,0 +1,90 @@
+package databaseServices;
+
+import databaseServices.io.UsersDatabase;
+import databaseServices.io.UsersDatabaseMapImpl;
+import databaseServices.service.UserService;
+import databaseServices.service.UserServiceImpl;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.AssertJUnit.assertNotNull;
+import static org.testng.AssertJUnit.assertNull;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class UserServiceImplTest {
+    UsersDatabase usersDatabase;
+    UserService userService;
+    String createdUserId;
+
+
+    @BeforeAll
+    void setup() { // Create & initialize database
+        usersDatabase = new UsersDatabaseMapImpl();
+        usersDatabase.init();
+        userService = new UserServiceImpl(usersDatabase);
+    }
+
+    @AfterAll
+    void cleanup() {// Close connection(Delete database)
+        usersDatabase.close();
+    }
+
+    @Test
+    @Order(1)
+    @DisplayName("Create User works")
+    void testCreateUser_whenProvidedWithValidDetails_returnsUserId() {
+        Map<String, String> user = new HashMap<>();
+        user.put("firstName", "A");
+        user.put("lastName", "B");
+
+        createdUserId = userService.createUser(user);
+
+        assertNotNull(createdUserId, "User id should not be null");
+        }
+
+
+    @Test
+    @Order(2)
+    @DisplayName("Update user works")
+    void testUpdateUser_whenProvidedWithValidDetails_returnsUpdatedUserDetails() {
+        Map<String, String> newUserDetails = new HashMap<>();
+        newUserDetails.put("firstName", "C");
+        newUserDetails.put("lastName", "B");
+
+        Map updateUserDetails = userService.updateUser(createdUserId, newUserDetails);
+        System.out.println(updateUserDetails.get("firstName") );
+        System.out.println(newUserDetails );
+
+//        Assertions.asser
+        assertEquals(newUserDetails.get("firstName"), (String)updateUserDetails.get("firstName"));
+        assertEquals(newUserDetails.get("lastName"), updateUserDetails.get("lastName"));
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Find user works")
+    void testGetUserDetails_whenProvidedWithValidUserId_returnsUserDetails() {
+        Map userDetails = userService.getUserDetails(createdUserId);
+        assertNotNull(userDetails);
+        assertEquals(createdUserId, userDetails.get("userId"));
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("Delete user works")
+    void testDeleteUser_whenProvidedWithValidUserId_returnsUserDetails() {
+        userService.deleteUser(createdUserId);
+        assertNull(userService.getUserDetails(createdUserId));
+    }
+}
---
+++ src/test/java/testInstances/TestInstancePerClassTest.java
@@ -0,0 +1,39 @@
+package testInstances;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class TestInstancePerClassTest {
+    StringBuffer str = new StringBuffer();
+
+    @AfterEach
+    void afterEach(){
+        System.out.println("the state of test object is : " + str);
+    }
+
+    @Test
+    void TestA(){
+        System.out.println("Running test A");
+        str.append("1");
+    }
+
+    @Test
+    void TestB(){
+        System.out.println("Running test B");
+        str.append("2");
+    }
+
+    @Test
+    void TestC(){
+        System.out.println("Running test C");
+        str.append("3");
+    }
+
+    @Test
+    void TestD(){
+        System.out.println("Running test D");
+        str.append("4");
+    }
+}
